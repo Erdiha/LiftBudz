1b2999a2c389994bc8df7b79442f560cd1ca1645
your.user.name <erdiha@hotmail.com>
2023-01-17 13:17:59 -0800

    Initial commit

--- src/components/mainpageSegments.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function mainpageSegments() {
-  return <div></div>;
-}
-
-export default mainpageSegments;
--- src/pages/about.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function about() {
-  return <div>hey</div>;
-}
-
-export default about;
